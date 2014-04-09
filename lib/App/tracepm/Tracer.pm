3d85bd44a8f8d4fea894bdbe3e417782090ef431
Steven Haryanto (on Asus Zenbook) <stevenharyanto@gmail.com>
2014-04-10 02:11:12 +0700

    Add trace method: require

---
+++ lib/App/tracepm/Tracer.pm
@@ -0,0 +1,46 @@
+package App::tracepm::Tracer;
+
+# VERSION
+
+# saving CORE::GLOBAL::require doesn't work
+my $orig_require;
+
+sub import {
+    my $self = shift;
+
+    # already installed
+    return if $orig_require;
+
+    my $opts = {
+        workaround_log4perl => 1,
+    };
+    if (@_ && ref($_[0]) eq 'HASH') {
+        $opts = shift;
+    }
+
+    my $file = shift
+        or die "Usage: use App::tracerpm::Tracer '/path/to/output'";
+
+    open my($fh), ">", $file or die "Can't open $file: $!";
+
+    #$orig_require = \&CORE::GLOBAL::require;
+    *CORE::GLOBAL::require = sub {
+        my ($arg) = @_;
+        if ($INC{$arg}) {
+            if ($opts->{workaround_log4perl}) {
+                # Log4perl <= 1.43 still does 'eval "require $foo" or ...'
+                # instead of 'eval "require $foo; 1" or ...' so running will
+                # fail. this workaround makes require() return 1.
+                my $caller = caller;
+                return 1 if $caller =~ /^Log::Log4perl/;
+            }
+            return 0;
+        }
+        print $fh $arg, "\n" unless $arg =~ /\A\d/; # skip 'require 5.xxx'
+        #$orig_require->($arg);
+        CORE::require($arg);
+    };
+}
+
+1;
+# ABSTRACT: Trace module require to file
